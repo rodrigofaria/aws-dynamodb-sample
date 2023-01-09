bb50eaef1fa291d386d04657f4e26a55e648d941
Rodrigo Faria <rodrigolfsi@gmail.com>
2023-01-09 19:19:17 -0300

    Add project structure

---
+++ src/main/java/br/com/rodrigofaria/awsdynamodbsample/AwsDynamodbSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigofaria.awsdynamodbsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDynamodbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDynamodbSampleApplication.class, args);
+	}
+
+}
